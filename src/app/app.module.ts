aed2fbb27d28dbaeedc0be47758a6af0a6f127c4
S.Zheleznov <S.Zheleznov@thomsonreuters.com>
2018-11-05 18:09:32 +0300

    added changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './material/material.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {AppRouters} from './app.routes';
+import { DataService} from './data/data.service';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    DashboardComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    AppRouters
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {DataService} from '../data/data.service';
+import {Post} from '../Post';
+import {DataSource} from '@angular/cdk/table';
+import {Observable} from 'rxjs/Observable';
+
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+
+  constructor(private dataService: DataService) { }
+
+    displayedColumns = ['date_posted', 'title', 'category', 'delete'];
+    dataSource = new PostDataSource(this.dataService);
+}
+
+export class PostDataSource extends DataSource<any> {
+  constructor(private dataService: DataService) {
+    super();
+  }
+  connect(): Observable<Post[]> {
+    return this.dataService.getData();
+  }
+
+  disconnect() {
+
+  }
+
+}
